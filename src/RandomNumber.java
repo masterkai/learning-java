72d21c70e9833d6a55702d0548cfba6ea959c86a
maxLiu <neoliukai@gmail.com>
2024-01-03 16:32:43 +0800

    feat(RandomNumber): add a class to generate a random number
    - Create a new file RandomNumber.java in src folder
    - Use java.util.Random to generate a random number between 0 and 100
    - Print the random number to the console

---
+++ src/RandomNumber.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        // Creating a Random object
+        Random random = new Random();
+        int min = 0;
+        int max = 100;
+        // Generating a random number between 0 (inclusive) and 99 (exclusive)
+        int secret = random.nextInt((max - min)+min);
+
+
+        System.out.println("Random Number between 0 and 100: " + secret);
+    }
+}
