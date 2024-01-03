bf7a979a75ca9983abf612a0c08c0d9c69409b46
maxLiu <neoliukai@gmail.com>
2024-01-03 10:08:03 +0800

    add: chapter 1 - BMI calculator

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static final DecimalFormat DF = new DecimalFormat("0.00");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("請輸入體重（kg）：");
+        double weight = scanner.nextDouble();
+        System.out.println("請輸入身高（公分）：");
+        double height = scanner.nextDouble() / 100;
+        double BMI = Double.parseDouble(DF.format(weight / (height * height)));
+        // 創建 BigDecimal 對象
+        BigDecimal bd = new BigDecimal(BMI);
+
+        // 設定小數點位數和捨入方式
+        BigDecimal roundedNumber = bd.setScale(1, RoundingMode.HALF_UP);
+        System.out.print("Yor BMI is：" + BMI);
+        System.out.print("Yor BMI is：" + roundedNumber);
     }
 }
