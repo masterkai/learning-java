c6bd20912a2c711592d498eebc2003a46bbfe140
maxLiu <neoliukai@gmail.com>
2024-01-03 16:42:03 +0800

    feat(RandomNumber add user input and guess logic (main)
    - Add Scanner to get user input
    - Add while loop to check user guess against secret number
    - Add feedback messages for user

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -1,7 +1,9 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class RandomNumber {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         // Creating a Random object
         Random random = new Random();
         int min = 0;
@@ -11,5 +13,13 @@ public class RandomNumber {
 
 
         System.out.println("The Secret Number between 0 and 99 is --> " + secret);
+        while (true) {
+            System.out.println("Make Guess (between "+min+" and "+max+"): ");
+            int guess = s.nextInt();
+            if(guess == secret) {
+                System.out.println("You Win!! The secret is --> "+ secret);
+                break;
+            }
+        }
     }
 }
