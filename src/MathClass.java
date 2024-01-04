ab094dafa558190292eb3dd981c0f7e2aa225b2b
maxLiu <neoliukai@gmail.com>
2024-01-04 14:10:21 +0800

    feat(MathClass): add basic math operations (#1)
    - Add MathClass.java file to src folder
    - Use Math class methods to perform max, sqrt, pow and log10
    - Print the results to the console

---
+++ src/MathClass.java
@@ -0,0 +1,12 @@
+public class MathClass {
+    public static void main(String[] args) {
+        int a = 28;
+        int b = 4;
+        double z = 3.14;
+
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.sqrt(a));
+        System.out.println(Math.pow(b, 3));
+        System.out.println(Math.log10(a));
+    }
+}
